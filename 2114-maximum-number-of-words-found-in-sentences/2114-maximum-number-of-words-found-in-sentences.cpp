856c95c579551dfe24d48b8a3647a3621e6d87bc
ArynAgarwal <96115326+ArynAgarwal@users.noreply.github.com>
2023-02-28 11:10:46 +0530

    Time: 18 ms (26.57%), Space: 10.1 MB (39.60%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
-    int mostWordsFound(vector<string>& sentences) {
-        int count=0,ans=0;
-        // int n = sizeof(count) / sizeof(count[0]);
-        for(int i=0;i<sentences.size();i++){
-            count=1;
-            for(int j=0;j<sentences[i].length();j++){
-                if(sentences[i][j]==' '){
-                    count++;
-                    // cout<<count[i]<<endl;
-                }
-            }
-            
-            ans=max(count,ans);
+    int mostWordsFound(vector<string>& sen) {
+        int mcount = 0;
+        for(auto x:sen){
+            mcount = max(mcount, (int)count(x.begin(),x.end(),' '));
         }
-        // for(int i=0;i<n;i++){
-        //     // cout<<count[i]<<"-";
-        // }
-        return ans;
+        return mcount+1;
     }
 };
