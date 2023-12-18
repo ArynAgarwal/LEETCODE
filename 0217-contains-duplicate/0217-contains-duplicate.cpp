9d8bf42bfdbe616bb5fafa9a3f73626ae214dd6d
ArynAgarwal <96115326+ArynAgarwal@users.noreply.github.com>
2023-12-18 17:36:08 +0530

    Time: 113 ms (29.58%), Space: 72.3 MB (10.10%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,21 +1,13 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            cout<<nums[i]<<" ";
-        }
-        bool a;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                // cout<<nums[i];
-                a= true;
-                break;
-            }
-            else 
-                // cout<<nums[i];
-                a= false;
-        }
-        return a;
-    }
-};
+  class Solution {
+  public:
+      bool containsDuplicate(vector<int>& nums) {
+          unordered_map<int,int> mp;
+          for(auto i : nums) 
+              mp[i]++;
+          bool flag = false;
+          for(auto i : mp){
+              if(i.second >= 2) return true;
+          }
+          return flag;
+      }
+  };
